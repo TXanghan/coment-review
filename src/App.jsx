e6b5abdae36156b8de939cc84cd4f9252b910343
TXanghan <Anghantaxa1210@gmail.com>
2024-09-09 12:18:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './components/form'
+import Form2 from './components/form2'
+import Feedback from './components/Feedback'
+
+function App() {
+  return (
+    <div>
+      {/* <Form/> */}
+      {/* <Form2/> */}
+      <Feedback/>
+    </div>
+  )
+}
+
+export default App
